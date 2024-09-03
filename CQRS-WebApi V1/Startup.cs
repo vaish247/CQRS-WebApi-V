160220a8188f5440e09116ecf04b933f49c9a4f1
ajithvais <ajithvais@myvuw.ac.nz>
2024-09-03 15:19:50 +1200

    Integrate Service Injector into Startup

--- CQRS-WebApi V1/Startup.cs
+++ CQRS-WebApi V1/Startup.cs
@@ -1,5 +1,12 @@
 ﻿using Microsoft.OpenApi.Models;
 using CQRS_WebApi_V1.Application;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using AutoMapper;
+using CQRS_WebApi_V1.Application.Handlers;
+using CQRS_WebApi_V1.Domain.Interfaces;
+using CQRS_WebApi_V1.Repository.Repositories;
+using CQRS_WebApi_V1.Application.Mapping;
 
 namespace CQRS_WebApi_V1.WebApi
 {
@@ -54,6 +61,42 @@ namespace CQRS_WebApi_V1.WebApi
             {
                 endpoints.MapControllers();
             });
+        }
+    }
+
+    public static class ServiceInjection
+    {
+        public static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            // Register DynamoDB client and context
+            services.AddSingleton<IAmazonDynamoDB>(sp =>
+            {
+                return new AmazonDynamoDBClient();
+            });
+
+            services.AddSingleton<IDynamoDBContext>(sp =>
+            {
+                var client = sp.GetRequiredService<IAmazonDynamoDB>();
+                return new DynamoDBContext(client);
+            });
+
+            // Register repositories
+            services.AddSingleton<IContactItemRepository, ContactItemRepository>();
+
+            // Register MediatR
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(AddContactItemHandler).Assembly));
+
+            // Register AutoMapper
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+            var mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            return services;
         }
     }
 }
